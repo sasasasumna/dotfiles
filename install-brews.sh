41dcf318820b57d2baabb929dc2094c0a053c665
Adam Sumner <adamsumner@gmail.com>
2015-10-20 22:26:26 -0700

    install not reinstall

--- install-brews.sh
+++ install-brews.sh
@@ -1,49 +1,48 @@
 #! /usr/local/bin/zsh
 
-# for install Brew: ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/reinstall/master/install)"
+# for install Brew: ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 # assuming brew, zsh, zsh-completions, zsh-syntax-highlighting already installed!
 brew update
 
-brew reinstall caskroom/cask/brew-cask
-brew cask reinstall xquartz
+brew install caskroom/cask/brew-cask
+brew cask install xquartz
 brew cleanup
 brew cask cleanup
 
-brew reinstall apple-gcc42 --with-gfortran-symlink
-brew reinstall homebrew/dupes/tcl-tk --with-x11 --with-threads
-brew reinstall sqlite --with-dbstat --with-docs --with-fts --with-functions --with-icu4c --with-unlock-notify
-brew reinstall python3 --with-tcl-tk
-brew reinstall python --with-tcl-tk --with-berkeley-db4
-brew reinstall lua --with-completion
-brew reinstall little-cms --with-python
-brew reinstall perl --with-dtrace
-brew reinstall jbig2dec --with-libpng
-brew reinstall harfbuzz --with-cairo
-brew reinstall gtk+3 --with-jasper --with-quartz-relocation
-brew reinstall librsvg --with-gtk+3
-brew reinstall graphviz --with-freetype --with-librsvg --with-pango --with-x11 --with-app
-brew reinstall yasm --with-python
-brew reinstall gd --with-libvpx # has issue with El Capitan
-brew reinstall open-mpi --c++11 --with-mpi-thread-multiple
-brew reinstall boost --with-c++11 --with-icu4c --with-mpi
-brew reinstall curl --with-c-ares --with-gssapi --with-libidn --with-libmetalink --with-nghttp2 --with-rtmpdump --with-libssh2 --with-openssl
-brew reinstall fftw --with-fortran --with-mpi --with-openmp
-brew reinstall ghostscript --with-x11 --with-djvu
-brew reinstall postgresql --with-dtrace --with-python
-brew reinstall subversion --with-java --with-perl --with-python --with-ruby
-brew reinstall git --with-brewed-openssl --with-brewed-svn --with-blk-sha1 --with-brewed-curl --with-gettext --with-pcre --with-persistent-https
-brew reinstall go --with-cc-all
-brew reinstall imagemagick --with-fftw --with-fontconfig --with-ghostscript --with-hdri --with-jp2 --with-liblqr --with-librsvg --with-libwmf --with-little-cms --with-little-cms2 --with-openexr --with-openmp --with-pango --with-perl
-brew reinstall node --with-icu4c --with-openssl
-brew reinstall homebrew/science/r --with-pango --with-valgrind --with-x11 --with-openblas --with-valgrind
-brew reinstall wget --with-iri --with-pcre
-brew reinstall vim --override-system-vi --with-python3 --with-tcl --with-lua --with-luajit
-brew reinstall libxml2 --with-python
-brew reinstall llvm --with-clang --with-lld --with-lldb --with-python --with-rtti
-brew reinstall objective-caml --with-x11
-brew reinstall tomcat --with-fulldocs
-brew reinstall erlang --with-fop --with-native-libs
-brew reinstall nginx --with-gunzip --with-spdy --with-webdav
+brew install apple-gcc42 --with-gfortran-symlink
+brew install homebrew/dupes/tcl-tk --with-x11 --with-threads
+brew install sqlite --with-dbstat --with-docs --with-fts --with-functions --with-icu4c --with-unlock-notify
+brew install python3 --with-tcl-tk
+brew install python --with-tcl-tk --with-berkeley-db4
+brew install lua --with-completion
+brew install little-cms --with-python
+brew install perl --with-dtrace
+brew install jbig2dec --with-libpng
+brew install harfbuzz --with-cairo
+brew install gtk+3 --with-jasper --with-quartz-relocation
+brew install librsvg --with-gtk+3
+brew install graphviz --with-freetype --with-librsvg --with-pango --with-x11 --with-app
+brew install yasm --with-python
+brew install gd --with-libvpx # has issue with El Capitan
+brew install boost --with-c++11 --with-icu4c # has issue with El Capitan
+brew install curl --with-c-ares --with-gssapi --with-libidn --with-libmetalink --with-nghttp2 --with-rtmpdump --with-libssh2 --with-openssl
+brew install fftw --with-fortran --with-openmp
+brew install ghostscript --with-x11 --with-djvu
+brew install postgresql --with-dtrace --with-python
+brew install subversion --with-java --with-perl --with-python --with-ruby --universal
+brew install git --with-brewed-openssl --with-brewed-svn --with-blk-sha1 --with-brewed-curl --with-gettext --with-pcre --with-persistent-https
+brew install go --with-cc-all
+brew install imagemagick --with-fftw --with-fontconfig --with-ghostscript --with-hdri --with-jp2 --with-liblqr --with-librsvg --with-libwmf --with-little-cms --with-little-cms2 --with-openexr --with-openmp --with-pango --with-perl #issue on El Capitan
+brew install node --with-icu4c --with-openssl
+brew install homebrew/science/r --with-pango --with-x11 --with-openblas
+brew install wget --with-iri --with-pcre
+brew install vim --override-system-vi --with-python3 --with-tcl --with-lua --with-luajit
+brew install libxml2 --with-python
+brew install llvm --with-clang --with-python
+brew install objective-caml --with-x11
+brew install tomcat --with-fulldocs
+brew install erlang --with-fop --with-native-libs
+brew install nginx --with-gunzip --with-spdy --with-webdav
 
-brew reinstall ack cvs heroku-toolbelt json-c libyaml memcached mercurial mysql redis riak rbenv ruby-build rust elixir homebrew/versions/v8-315
-brew cask reinstall java android-studio ax88179 dropbox firefox gimp google-chrome google-drive google-earth google-earth-web-plugin google-hangouts inkscape hipchat iterm2 psequel skype slack steam sublime-text3 tunnelblick vagrant vagrant-manager virtualbox visual-studio-code vlc
+brew install ack cvs heroku-toolbelt json-c libyaml memcached mercurial mysql redis riak rbenv ruby-build rust elixir homebrew/versions/v8-315
+brew cask install java android-studio ax88179 dropbox firefox gimp google-chrome google-drive google-earth google-earth-web-plugin google-hangouts inkscape hipchat iterm2 psequel skype slack steam sublime-text3 tunnelblick vagrant vagrant-manager virtualbox visual-studio-code vlc
