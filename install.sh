48a1bb033f5c36df77b3cd74640135faa8f68a95
Adam Sumner <adam@sumner.us>
2025-02-06 15:25:02 -0500

    add basic cursor config

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ mkdir -p ~/.config/coc
 mkdir -p ~/.config/yarn/global
 mkdir -p ~/.config/alacritty
 mkdir -p ~/.config/ghostty
+mkdir -p ~/.config/Cursor/User
 mkdir -p ~/.zfunc
 mkdir -p ~/.ssh
 
@@ -28,6 +29,8 @@ ln -sf ~/.dotfiles/gemspec ~/.gemspec
 ln -sf ~/.dotfiles/sshconfig ~/.ssh/config
 ln -sf ~/.dotfiles/alacritty.toml ~/.config/alacritty/alacritty.toml
 ln -sf ~/.dotfiles/ghostty ~/.config/ghotty/config
+ln -sf ~/.dotfiles/cursor.json ~/.config/Cursor/User/settings.json
+
 
 ln -sf ~/.dotfiles/vimrc ~/.config/nvim/init.vim
 ln -sf ~/.dotfiles/erdconfig ~/.erdconfig
